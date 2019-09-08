1661498349f2c3923ea729a61b4c6ca26b12c2ab
wujialiang999 <1647113057@qq.com>
2019-09-08 16:23:24 +0800

    第五章函数-宕机

--- src/goStudy/ch05/eighteenth.go
+++ src/goStudy/ch05/eighteenth.go
@@ -1,15 +1,17 @@
 package main
 
+import "regexp"
+
 //宕机
-func main(){
+func main() {
 	panic("宕机了")
 }
 
 //运行所必需的资源缺失时触发宕机
-func mustCompile(str string) *Regexp{
-	regexp,err:=Complile(str)
-	if err!=nil{
-		panic(`regexp Compile(`+quote(str)+`):`+err.Error())
+func mustCompile(str string) *Regexp {
+	regexpResult, err := regexp.Complile(str)
+	if err != nil {
+		panic(`regexp Compile(` + quote(str) + `):` + err.Error())
 	}
-	retrurn regexp
+	return regexpResult
 }
