d8d82824d2ab87181c973e55599a25d684e19fe2
wujialiang999 <1647113057@qq.com>
2019-09-13 20:26:30 +0800

    第七章接口-数据写入器的抽象

---
+++ src/goStudy/ch07/dataWriter.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type DataWriter interface {
+	WriteData(data interface{}) error
+}
+
+type file struct {
+}
+
+func (d *file) WriteData(data interface{}) error {
+	fmt.Println("writeData:", data)
+	return nil
+}
+
+func main() {
+	f := new(file)
+	var writer DataWriter
+	writer = f
+	writer.WriteData("data")
+}
