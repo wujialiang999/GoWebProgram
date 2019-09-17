a207e8df656214e0d7ffc9c46ae32256c80d8eca
wujialiang <36027583+wujialiang999@users.noreply.github.com>
2019-09-17 19:37:59 +0800

    goroutine实例

---
+++ src/goStudy/ch09/first.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func running() {
+	var times int
+	for {
+		times++
+		fmt.Println("tick", times)
+		time.Sleep(time.Second)
+	}
+}
+func main() {
+	go running()
+	var input string
+	fmt.Scanln(&input)
+}
