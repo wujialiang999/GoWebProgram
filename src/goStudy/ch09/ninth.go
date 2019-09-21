28df358602c79f75a07b773ca6b9550536d68162
wujialiang999 <1647113057@qq.com>
2019-09-21 16:28:58 +0800

    第九章并发-一秒后触发

---
+++ src/goStudy/ch09/ninth.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	exit := make(chan int)
+	fmt.Println("start")
+	time.AfterFunc(time.Second, func() {
+		fmt.Println("one second after")
+		exit <- 0
+	})
+	<-exit
+}
