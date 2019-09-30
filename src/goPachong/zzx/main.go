635ab14a00fc26e5ac7653a01f8ecdf1f4776128
wujialiang999 <1647113057@qq.com>
2019-09-30 20:27:56 +0800

    网页分析

--- src/goPachong/zzx/main.go
+++ src/goPachong/zzx/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goPachong/zzx/httputils"
+)
 
 func main() {
 	fmt.Println("开始工作")
+	url := "https://jiaotong.baidu.com/trafficindex/city/list"
+	httputils.HTTPGet(url)
 }
