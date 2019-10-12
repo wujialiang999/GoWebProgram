91fee77b4424e8cdceb809716225c897388bb55a
wujialiang999 <1647113057@qq.com>
2019-10-12 20:05:27 +0800

    修改代码

--- src/goPachong/zzx/main.go
+++ src/goPachong/zzx/main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	"trafficcrawler"
+	"goPachong/zzx/trafficcrawler"
 )
 
 func main() {
-	// cityCodeURL := "https://jiaotong.baidu.com/trafficindex/city/list"
-	// body, err := trafficcrawler.HTTPGet(cityCodeURL)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// err = trafficcrawler.CreateCitycodeCSV(body, "citycode.csv")
-	// if err != nil {
-	// 	return
-	// }
-	// citycodes, err := trafficcrawler.GetCitycode("citycode.csv")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	citycodes := []string{"210", "79"}
+	cityCodeURL := "https://jiaotong.baidu.com/trafficindex/city/list"
+	body, err := trafficcrawler.HTTPGet(cityCodeURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = trafficcrawler.CreateCitycodeCSV(body, "citycode.csv")
+	if err != nil {
+		return
+	}
+	citycodes, err := trafficcrawler.GetCitycode("citycode.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// citycodes := []string{"210", "79"}
 	citychan := make(chan string)
 	for i := 0; i < len(citycodes); i++ {
 		go trafficcrawler.CreateCitydataCSV(citycodes[i], citychan)
